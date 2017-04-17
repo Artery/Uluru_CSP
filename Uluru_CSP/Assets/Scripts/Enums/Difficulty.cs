6acd8806308eabab433954d5757139b893da5c0b
Johannes Winkler <jw.artery22@web.de>
2017-04-17 16:31:20 +0200

    + adds base implemention for RuleCard and Difficulty + adds Enum folder ~ moves Color into Enum folder

---
+++ Uluru_CSP/Assets/Scripts/Enums/Difficulty.cs
@@ -0,0 +1,1 @@
+﻿public enum Difficulty { VERY_EASY = 1, EASY, MEDIUM, HARD, VERY_HARD };
---
+++ Uluru_CSP/Assets/Scripts/RuleCard.cs
@@ -0,0 +1,5 @@
+﻿public class RuleCard
+{
+    public Difficulty Difficulty { get; set; }
+    public Ruleset Ruleset { get; set; }
+}
