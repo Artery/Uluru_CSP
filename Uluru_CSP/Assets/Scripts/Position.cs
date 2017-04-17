b0d659db9f68a8abff3768d36e647949db3cd06a
Johannes Winkler <jw.artery22@web.de>
2017-04-17 16:08:54 +0200

    Base Implementation   + adds base implementation for Position

---
+++ Uluru_CSP/Assets/Scripts/Position.cs
@@ -0,0 +1,16 @@
+﻿public class Position
+{
+    protected static int nextIndex = 0;
+
+    public int Index { get; protected set; }
+
+    public Position()
+    {
+        Index = nextIndex++;
+    }
+
+    public void ResetIndices()
+    {
+        nextIndex = 0;
+    }
+}
