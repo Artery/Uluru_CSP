059afd2b940d38e9ad9851f4f38d4204c7cd5119
Johannes Winkler <jw.artery22@web.de>
2017-04-17 17:19:51 +0200

    + adds base implementation for Game, PlayerCollection and CardCollection

---
+++ Uluru_CSP/Assets/Scripts/Game.cs
@@ -0,0 +1,45 @@
+﻿public class Game
+{
+    private Gameplan m_gameplan = new Gameplan();
+    private Hourglass m_hourglass = new Hourglass();
+
+    private PlayerCollection m_players = new PlayerCollection();
+    private CardCollection m_deck = new CardCollection();
+    private CardCollection m_gamepile = new CardCollection();
+    private CardCollection m_discardpile = new CardCollection();
+
+
+    public PlayerCollection Players
+    {
+        get { return m_players; }
+    }
+
+    public CardCollection Deck
+    {
+        get { return m_deck; }
+    }
+
+    public CardCollection GamePile
+    {
+        get { return m_gamepile; }
+    }
+
+    public CardCollection DiscardPile
+    {
+        get { return m_discardpile; }
+    }
+
+    public Gameplan Gameplan
+    {
+        get { return m_gameplan; }
+    }
+
+    public Hourglass Hourglass
+    {
+        get { return m_hourglass; }
+    }
+
+    public Difficulty Difficulty { get; set; }
+    public int MaxRounds { get; set; }
+    public int CurrentRound { get; set; }
+}
