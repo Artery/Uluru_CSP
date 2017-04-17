2d95fc95196b4d40c986a3ea13f3ba223bd47287
Johannes Winkler <jw.artery22@web.de>
2017-04-17 16:14:14 +0200

    Base implementation   + adds base implementation of Token

---
+++ Uluru_CSP/Assets/Scripts/Token.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Token
+{
+    public Color color { get; set; }
+    public Position position { get; set; }
+
+    public Token(Color color, Position position = null)
+    {
+        this.color = color;
+        this.position = position;
+    }
+}
