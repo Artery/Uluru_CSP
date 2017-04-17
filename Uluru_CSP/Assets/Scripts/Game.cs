3e01e8f94679b06b7b051f5fa05307bfabca859b
Johannes Winkler <jw.artery22@web.de>
2017-04-17 17:45:22 +0200

    ~ changes property handling

--- Uluru_CSP/Assets/Scripts/Game.cs
+++ Uluru_CSP/Assets/Scripts/Game.cs
@@ -1,11 +1,26 @@
-﻿public class Game
+﻿using UnityEngine;
+
+public class Game : MonoBehaviour
 {
+    [SerializeField]
     private Gameplan m_gameplan = new Gameplan();
+    [SerializeField]
     private Hourglass m_hourglass = new Hourglass();
 
+    [SerializeField]
+    private Difficulty m_difficulty;
+    [SerializeField]
+    private int m_maxRounds;
+    [SerializeField]
+    private int m_currentRound;
+
+    [SerializeField]
     private PlayerCollection m_players = new PlayerCollection();
+    [SerializeField]
     private CardCollection m_deck = new CardCollection();
+    [SerializeField]
     private CardCollection m_gamepile = new CardCollection();
+    [SerializeField]
     private CardCollection m_discardpile = new CardCollection();
 
 
@@ -39,7 +54,19 @@
         get { return m_hourglass; }
     }
 
-    public Difficulty Difficulty { get; set; }
-    public int MaxRounds { get; set; }
-    public int CurrentRound { get; set; }
+    public Difficulty Difficulty
+    {
+        get { return m_difficulty; }
+        set { m_difficulty = value; }
+    }
+    public int MaxRounds
+    {
+        get { return m_maxRounds; }
+        set { m_maxRounds = value; }
+    }
+    public int CurrentRound
+    {
+        get { return m_currentRound; }
+        set { m_currentRound = value; }
+    }
 }
