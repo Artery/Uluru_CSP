f6ce3e3ead556bf7650fd6b4b21ff5772da81371
Johannes Winkler <jw.artery22@web.de>
2017-04-17 16:25:57 +0200

    Base implementation   + adds base implementation for Player and TokenCollection

---
+++ Uluru_CSP/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    private TokenCollection m_tokens = new TokenCollection();
+
+    public string Name { get; set; }
+    public int Drawback { get; set; }
+    public Gameboard Gameboard { get; set; }
+    public TokenCollection Tokens
+    {
+        get
+        {
+            return m_tokens;
+        }
+     }
+}
