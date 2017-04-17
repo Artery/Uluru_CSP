1f291500ab4c13e56cc32278e17583ec0c6173b9
Johannes Winkler <jw.artery22@web.de>
2017-04-17 17:38:25 +0200

    ~ changes property handling

--- Uluru_CSP/Assets/Scripts/Player.cs
+++ Uluru_CSP/Assets/Scripts/Player.cs
@@ -2,18 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player
+public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private string m_name;
+    [SerializeField]
+    private int m_drawback;
+    [SerializeField]
+    private Gameboard m_gameboard = new Gameboard();
+    [SerializeField]
     private TokenCollection m_tokens = new TokenCollection();
 
-    public string Name { get; set; }
-    public int Drawback { get; set; }
-    public Gameboard Gameboard { get; set; }
+    public string Name
+    {
+        get { return m_name; }
+        set { m_name = value; }
+    }
+    public int Drawback
+    {
+        get { return m_drawback; }
+        set { m_drawback = value; }
+    }
+    public Gameboard Gameboard
+    {
+        get { return m_gameboard; }
+        set { m_gameboard = value; }
+    }
     public TokenCollection Tokens
     {
-        get
-        {
-            return m_tokens;
-        }
-     }
+        get { return m_tokens; }
+    }
 }
