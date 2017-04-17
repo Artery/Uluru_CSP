da81a38165553f03298aed9b1409a22d35c2be9e
Johannes Winkler <jw.artery22@web.de>
2017-04-17 17:32:51 +0200

    ~ changes property handling

--- Uluru_CSP/Assets/Scripts/RuleCard.cs
+++ Uluru_CSP/Assets/Scripts/RuleCard.cs
@@ -1,5 +1,20 @@
-﻿public class RuleCard
+﻿using UnityEngine;
+
+public class RuleCard : MonoBehaviour
 {
-    public Difficulty Difficulty { get; set; }
-    public Ruleset Ruleset { get; set; }
+    [SerializeField]
+    private Difficulty m_difficulty;
+    [SerializeField]
+    private Ruleset m_ruleset;
+
+    public Difficulty Difficulty
+    {
+        get { return m_difficulty; }
+        set { m_difficulty = value; }
+    }
+    public Ruleset Ruleset
+    {
+        get { return m_ruleset; }
+        set { m_ruleset = value; }
+    }
 }
