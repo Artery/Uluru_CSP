0c6727c4535ebd149cfcd99ed2d0bb94328c85da
Johannes Winkler <jw.artery22@web.de>
2017-04-17 16:45:11 +0200

    ~ refactors property names + adds comments

--- Uluru_CSP/Assets/Scripts/Token.cs
+++ Uluru_CSP/Assets/Scripts/Token.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//This class represents one Token, each Player will have 7
+//Each Token has to be placed on one unique Position
 public class Token
 {
-    public Color color { get; set; }
-    public Position position { get; set; }
+    public Color Color { get; set; }
+    public Position Position { get; set; }
 
     public Token(Color color, Position position = null)
     {
-        this.color = color;
-        this.position = position;
+        this.Color = color;
+        this.Position = position;
     }
 }
