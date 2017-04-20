ecbeb898ae5b6c35582cb88cefffc9dba0b13b03
Johannes Winkler <jw.artery22@web.de>
2017-04-20 23:44:57 +0200

    + implements methods used by Game (except DealOutNextSequence)

--- Uluru_CSP/Assets/Scripts/Gameplan.cs
+++ Uluru_CSP/Assets/Scripts/Gameplan.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -25,26 +26,47 @@ public class Gameplan : MonoBehaviour
 
     public void Intialize(CardCollection gameDeck)
     {
-        throw new NotImplementedException();
+        GamePile.AddRange(gameDeck);
     }
 
+    //Clears the whole Gameplan and it's piles
     public void Clear()
     {
-        throw new NotImplementedException();
+        GamePile.Clear();
+        DiscardPile.Clear();
+
+        ResetSlots();
     }
 
+    //Resets the slots, by removing the RuleCards from each color
+    public void ResetSlots()
+    {
+        foreach (var color in Slots) { Slots[color.Key] = null; }
+    }
+
+    //Fills the slots with RuleCards from the GamePile
+    //Only fills "intern" Slots, to make the new assignments visible for the players
+    //use DealOutNextSequence
     public void GenerateSequence()
     {
-        throw new NotImplementedException();
+        foreach(var slot in Slots)
+        {
+            Slots[slot.Key] = GamePile.FirstOrDefault();
+            GamePile.RemoveAt(0);
+        }
     }
 
+    //Makes the current "intern" Slots visible to the players
     public void DealOutNextSequence()
     {
         throw new NotImplementedException();
     }
 
+    //Resets the Gameplan by removing the RuleCards from the Slots
+    //and adding them to the DiscardPile
     public void Reset()
     {
-        throw new NotImplementedException();
+        DiscardPile.AddRange(Slots.Select(slot => slot.Value));
+        ResetSlots();
     }
 }
