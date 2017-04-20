d5e9c6953730441dd47bb6e4f27f0148e7d8cdef
Johannes Winkler <jw.artery22@web.de>
2017-04-21 00:44:09 +0200

    +implements Reset method

--- Uluru_CSP/Assets/Scripts/Gameboard.cs
+++ Uluru_CSP/Assets/Scripts/Gameboard.cs
@@ -35,6 +35,6 @@ public class Gameboard : MonoBehaviour
 
     public void Reset()
     {
-        throw new NotImplementedException();
+        m_positionsTokens.ForEach(tuple => tuple.Token = null);
     }
 }
