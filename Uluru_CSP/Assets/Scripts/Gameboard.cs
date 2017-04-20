d67508bebf08db6e6710e342dcbe719271b17557
Johannes Winkler <jw.artery22@web.de>
2017-04-21 00:41:55 +0200

    ~changes Gameboard PositionCollection to PositionTokenTupleCollection

--- Uluru_CSP/Assets/Scripts/Gameboard.cs
+++ Uluru_CSP/Assets/Scripts/Gameboard.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,29 @@ using UnityEngine;
 public class Gameboard : MonoBehaviour
 {
     [SerializeField]
-    private PositionCollection m_positions = new PositionCollection();
+    private PositionTokenTupleCollection m_positionsTokens = new PositionTokenTupleCollection();
+
+    public PositionTokenTupleCollection PositionsTokens
+    {
+        get
+        {
+            return m_positionsTokens;
+        }
+    }
 
     public PositionCollection Positions
     {
         get
         {
-            return m_positions;
+            return new PositionCollection(m_positionsTokens.Select(tuple => tuple.Postion));
+        }
+    }
+
+    public TokenCollection Tokens
+    {
+        get
+        {
+            return new TokenCollection(m_positionsTokens.Select(tuple => tuple.Token));
         }
     }
 
