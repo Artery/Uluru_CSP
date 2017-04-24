31e22d9f30305d9274c2f548fe1517ca90fce7f7
Johannes Winkler <jw.artery22@web.de>
2017-04-24 10:34:37 +0200

    VerifyBoardState   + adds first approach to verfiy BoardState   + adds needed method & property stub for Ruleset

--- Uluru_CSP/Assets/Scripts/Gameboard.cs
+++ Uluru_CSP/Assets/Scripts/Gameboard.cs
@@ -37,4 +37,28 @@ public class Gameboard : MonoBehaviour
     {
         m_positionsTokens.ForEach(tuple => tuple.Token = null);
     }
+
+    public List<Color> VerifyBoardState(Color_CardMap gameplanState)
+    {
+        var wrongTokens = new List<Color>();
+
+        foreach(var color_card in gameplanState)
+        {
+            PositionTokenTuple rulesetTuple = null;
+            var slotTuple = m_positionsTokens.FirstOrDefault(tuple => tuple.Token != null && tuple.Token.Color.Equals(color_card.Key));
+            var ruleset = color_card.Value.Ruleset;
+
+            if (!ruleset.Color.Equals(Color.NONE))
+            {
+                rulesetTuple = m_positionsTokens.FirstOrDefault(tuple => tuple.Token != null && tuple.Token.Color.Equals(ruleset.Color));
+            }
+
+            if(!ruleset.VerfiyRuleset(slotTuple, rulesetTuple))
+            {
+                wrongTokens.Add(color_card.Key);
+            }
+        }
+
+        return wrongTokens;
+    }
 }
--- Uluru_CSP/Assets/Scripts/Ruleset.cs
+++ Uluru_CSP/Assets/Scripts/Ruleset.cs
@@ -1,5 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Ruleset
 {
+    public Color Color { get; set; }
+
+    public bool VerfiyRuleset(PositionTokenTuple slotTuple, PositionTokenTuple rulesetTuple)
+    {
+        throw new NotImplementedException();
+    }
 }
