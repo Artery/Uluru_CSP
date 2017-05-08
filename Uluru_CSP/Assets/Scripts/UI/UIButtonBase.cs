8601040cad7a856e06526282aafbdaa275875197
Johannes Winkler <jw.artery22@web.de>
2017-05-08 19:57:22 +0200

    + adds UIButtonBase, which holds base logic for UIButtons (e.g. Tokens & Positions)

---
+++ Uluru_CSP/Assets/Scripts/UI/UIButtonBase.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIButtonBase : MonoBehaviour
+{
+    [SerializeField]
+    protected bool m_IsUnlocked;
+    [SerializeField]
+    protected Button m_Button;
+
+    public Button Button
+    {
+        get
+        {
+            return m_Button;
+        }
+
+        protected set
+        {
+            m_Button = value;
+        }
+    }
+
+    public bool IsUnlocked
+    {
+        get
+        {
+            return m_IsUnlocked;
+        }
+
+        set
+        {
+            if (m_IsUnlocked != value)
+            {
+                m_IsUnlocked = value;
+
+                m_Button.interactable = m_IsUnlocked;
+            }
+        }
+    }
+}
