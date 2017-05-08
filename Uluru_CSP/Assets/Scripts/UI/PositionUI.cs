ae8c27ee155aaf7924ad2dcf98801d09ebb402ce
Johannes Winkler <jw.artery22@web.de>
2017-05-08 11:05:30 +0200

    +adds basic approach for handling the placing of tokens (UI side only)

---
+++ Uluru_CSP/Assets/Scripts/UI/PositionUI.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PositionUI : MonoBehaviour
+{
+
+    [SerializeField]
+    private Image m_TokenImage;
+    private TokenUI m_UIToken;
+
+    private Position m_position;
+
+
+    public Position Position
+    {
+        get
+        {
+            return m_position;
+        }
+
+        set
+        {
+            m_position = value;
+        }
+    }
+
+    public Image TokenImage
+    {
+        get
+        {
+            return m_TokenImage;
+        }
+
+        protected set
+        {
+            m_TokenImage = value;
+        }
+    }
+
+    public TokenUI UIToken
+    {
+        get
+        {
+            return m_UIToken;
+        }
+
+        set
+        {
+            m_UIToken = value;
+        }
+    }
+
+    public void SetToken(TokenUI token)
+    {
+        if(token != null)
+        {
+            if(m_UIToken != null)
+            {
+                m_UIToken.SetPosition(null);
+            }
+
+            m_UIToken = token;
+            m_TokenImage.enabled = true;
+            m_TokenImage.color = token.Color;
+        }
+        else
+        {
+            m_UIToken = null;
+            m_TokenImage.enabled = false;
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Uluru_CSP/Assets/Scripts/UI/TokenUI.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TokenUI : MonoBehaviour
+{
+    private PositionUI m_UIPosition;
+
+    private UnityEngine.Color m_TokenColor;
+
+    public UnityEngine.Color Color
+    {
+        get
+        {
+            return m_TokenColor;
+        }
+
+        protected set
+        {
+            m_TokenColor = value;
+        }
+    }
+
+    public PositionUI UIPosition
+    {
+        get
+        {
+            return m_UIPosition;
+        }
+
+        set
+        {
+            m_UIPosition = value;
+        }
+    }
+
+    public void SetPosition(PositionUI position)
+    {
+        if (position != null)
+        {
+            if (m_UIPosition != null)
+            {
+                m_UIPosition.SetToken(null);
+            }
+
+            m_UIPosition = position;
+        }
+        else
+        {
+            m_UIPosition = null;
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_TokenColor = this.GetComponent<Button>().colors.normalColor;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+}
