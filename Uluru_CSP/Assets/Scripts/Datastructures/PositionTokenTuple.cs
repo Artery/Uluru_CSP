3b8f83ac50e59bccb6fac3acf70dfd3285a24118
Johannes Winkler <jw.artery22@web.de>
2017-05-08 20:38:09 +0200

    ~ changes property handling ~ makes class serializable

--- Uluru_CSP/Assets/Scripts/Datastructures/PositionTokenTuple.cs
+++ Uluru_CSP/Assets/Scripts/Datastructures/PositionTokenTuple.cs
@@ -2,14 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class PositionTokenTuple
-{ 
-    public Position Position { get; set; }
-    public Token Token { get; set; }
+{
+    [SerializeField]
+    private Position m_Position;
+    [SerializeField]
+    private Token m_Token;
 
     public PositionTokenTuple(Position position, Token token)
     {
         Position = position;
         Token = token;
     }
+
+    public Position Position
+    {
+        get
+        {
+            return m_Position;
+        }
+
+        set
+        {
+            m_Position = value;
+        }
+    }
+
+    public Token Token
+    {
+        get
+        {
+            return m_Token;
+        }
+
+        set
+        {
+            m_Token = value;
+        }
+    }
 }
+
