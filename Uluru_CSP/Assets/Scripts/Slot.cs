5f8e44b548d1883906b734354c9680923f90d07d
Johannes Winkler <jw.artery22@web.de>
2017-05-09 14:04:31 +0200

    + adds base implementation of Slot class (will replace Color_CardMap in Gameplan later)

---
+++ Uluru_CSP/Assets/Scripts/Slot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    [SerializeField]
+    private Color m_Color;
+    [SerializeField]
+    private RuleCard m_RuleCard;
+
+    public Color Color
+    {
+        get { return m_Color; }
+        set { m_Color = value; }
+    }
+
+    public RuleCard RuleCard
+    {
+        get { return m_RuleCard; }
+        set { m_RuleCard = value; }
+    }
+}
