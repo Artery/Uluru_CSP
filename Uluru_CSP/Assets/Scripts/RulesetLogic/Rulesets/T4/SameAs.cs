7c075bde67c0fb0dac81033529253df0b7f924a1
Johannes Winkler <jw.artery22@web.de>
2017-05-16 00:06:16 +0200

    + adds stub for SameAs + adds stub for OppositeOf

---
+++ Uluru_CSP/Assets/Scripts/RulesetLogic/Rulesets/T4/SameAs.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SameAs : IRulesetLogic
+{
+    private TokenABS m_Abs;
+
+    public void Initialize(PositionTokenTuple origin, PositionTokenTuple dependecy)
+    {
+        
+        /*if (origin != null && dependecy != null)
+        {
+            int dIndex = dependecy.Position.Index;
+            int oIndex = origin.Position.Index;
+
+            m_Abs = new TokenABS(TokenABS.enComparer.GREATER, 1, dIndex, oIndex);
+        }
+        else
+        {
+            m_Abs = null;
+        }*/
+    }
+
+    public bool Evaluate()
+    {
+        return false;
+        //return m_Abs != null ? m_Abs.Evaluate() : false;
+    }
+}
