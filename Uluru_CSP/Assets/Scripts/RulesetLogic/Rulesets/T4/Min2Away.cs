ec839eb01943dfa1ca1fbdc365b740f18b2533e0
Johannes Winkler <jw.artery22@web.de>
2017-05-15 19:55:28 +0200

    + adds T4 ruleset Min2Away + adds T4 ruleset NotAdjacentNotOpposite

---
+++ Uluru_CSP/Assets/Scripts/RulesetLogic/Rulesets/T4/Min2Away.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Min2Away : IRulesetLogic
+{
+    public Position OriginPosition { get; set; }
+    public Position DependencyPosition { get; set; }
+    private TokenABS m_Abs;
+
+    public Min2Away(Position dependencyPosition = null, Position originPosition = null)
+    {
+        OriginPosition = originPosition;
+        DependencyPosition = dependencyPosition;
+
+        int? dIndex = DependencyPosition != null ? (int?)DependencyPosition.Index : null;
+        int? oIndex = OriginPosition != null ? (int?)OriginPosition.Index : null;
+
+        m_Abs = new TokenABS(TokenABS.enComparer.GREATER, 1, dIndex, oIndex);
+    }
+
+    public bool Evaluate()
+    {
+        return m_Abs != null ? m_Abs.Evaluate() : false;
+    }
+}
---
+++ Uluru_CSP/Assets/Scripts/RulesetLogic/Rulesets/T4/NotAdjacentNotOpposite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotAdjacentNotOpposite : IRulesetLogic
+{
+    public Adjacent Adjacent { get; set; }
+    public Opposite Opposite { get; set; }
+    private LogicNot m_lhsNot;
+    private LogicNot m_rhsNot;
+    private LogicAnd m_And;
+
+    public NotAdjacentNotOpposite(Adjacent adjacent = null, Opposite opposite = null)
+    {
+        Adjacent = adjacent;
+        Opposite = opposite;
+
+        m_lhsNot = new LogicNot(Adjacent);
+        m_rhsNot = new LogicNot(Opposite);
+
+        m_And = new LogicAnd(m_lhsNot, m_rhsNot);
+    }
+
+    public bool Evaluate()
+    {
+        return m_And != null ? m_And.Evaluate() : false;
+    }
+}
