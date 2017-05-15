9d5e477a6e70128dc5bac430a5964ffe9d3d0eae
Johannes Winkler <jw.artery22@web.de>
2017-05-15 21:12:20 +0200

    + adds BasicLogic interfaces, will be implemented by every Ruleset class later

---
+++ Uluru_CSP/Assets/Scripts/RulesetLogic/Interfaces/IBasicLogic.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBasicLogic
+{
+    //void Initialize(PositionTokenTuple origin, PositionTokenTuple dependecy);
+
+    bool Evaluate();
+}
