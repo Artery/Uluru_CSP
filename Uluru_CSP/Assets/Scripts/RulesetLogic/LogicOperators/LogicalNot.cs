830f708fe0cc3fd8b5e6ccf93cfa312a8505dfca
Johannes Winkler <jw.artery22@web.de>
2017-05-16 18:08:59 +0200

    + adds LogicalNot operator

---
+++ Uluru_CSP/Assets/Scripts/RulesetLogic/LogicOperators/LogicalNot.cs
@@ -0,0 +1,14 @@
+﻿public class LogicalNot : IEvaluable
+{
+    public IEvaluable Object { get; set; }
+
+    public LogicalNot(IEvaluable obj)
+    {
+        Object = obj;
+    }
+
+    public bool Evaluate()
+    {
+        return Object != null ? !Object.Evaluate() : false;
+    }
+}
