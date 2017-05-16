9c4ebdf653606066793ad41a5f47ea860821a9cb
Johannes Winkler <jw.artery22@web.de>
2017-05-16 14:27:58 +0200

    + adds Inverse RulesetLogic

---
+++ Uluru_CSP/Assets/Scripts/RulesetLogic/Rulesets/Inverse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inverse : IRulesetLogic
+{
+    private IRulesetLogic m_RulesetLogic;
+
+    public Inverse(IRulesetLogic rulesetLogic)
+    {
+        m_RulesetLogic = rulesetLogic;
+    }
+
+    public void Initialize(PositionTokenTuple origin, PositionTokenTuple dependecy)
+    {
+    }
+
+    public bool Evaluate()
+    {
+        return !m_RulesetLogic.Evaluate();
+    }
+}
