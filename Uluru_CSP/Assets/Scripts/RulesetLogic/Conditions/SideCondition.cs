74df80d35ec03c53a7ab33997b912b2204a54b66
Johannes Winkler <jw.artery22@web.de>
2017-05-16 18:10:10 +0200

    + adds SideCondition

---
+++ Uluru_CSP/Assets/Scripts/RulesetLogic/Conditions/SideCondition.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class SideCondition : IEvaluable
+{
+    public enLogicComparator Comparator { get; set; }
+
+    public Edge.enSide LhsSide { get; set; }
+    public Edge.enSide RhsSide { get; set; }
+
+    public SideCondition(enLogicComparator comparator, Edge.enSide lhsSide, Edge.enSide rhsSide)
+    {
+        Comparator = comparator;
+        LhsSide = lhsSide;
+        RhsSide = rhsSide;
+    }
+
+    public bool Evaluate()
+    {
+        var result = false;
+
+        switch (Comparator)
+        {
+            case enLogicComparator.EQUALS:
+                result = LhsSide == RhsSide;
+                break;
+            case enLogicComparator.UNEQUALS:
+                result = LhsSide != RhsSide;
+                break;
+        }
+
+        return result;
+    }
+}
