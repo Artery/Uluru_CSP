055343f070ef3050e44734972e5fd963b24f117a
Johannes Winkler <jw.artery22@web.de>
2017-05-16 18:11:58 +0200

    + adds T3 Ruleset Adjacent

---
+++ Uluru_CSP/Assets/Scripts/RulesetLogic/Rulesets/T3/Adjacent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adjacent: IRuleset
+{
+    private DistanceCondition m_DistanceCondition;
+    private EdgeCondition m_EdgeCondition;
+    private LogicalAnd m_And;
+
+    public void Initialize(PositionTokenTuple slotTuple, PositionTokenTuple rulesetTuple)
+    {
+        if (slotTuple != null && rulesetTuple != null)
+        {
+            int slotTokenPositionIndex = slotTuple.Position.Index;
+            int rulesetTokenPositionIndex = rulesetTuple.Position.Index;
+
+            Edge.enEdgeID slotTokenPositionEdge = slotTuple.Position.Edge.EdgeID;
+            Edge.enEdgeID rulesetTokenPositionEdge = rulesetTuple.Position.Edge.EdgeID;
+
+            m_DistanceCondition = new DistanceCondition(enLogicComparator.EQUALS, 1, slotTokenPositionIndex, rulesetTokenPositionIndex);
+            m_EdgeCondition = new EdgeCondition(enLogicComparator.EQUALS, slotTokenPositionEdge, rulesetTokenPositionEdge);
+
+            m_And = new LogicalAnd(m_DistanceCondition, m_EdgeCondition);
+        }
+        else
+        {
+            m_And = null;
+        }
+    }
+
+    public bool Evaluate()
+    {
+        return m_And != null ? m_And.Evaluate() : false;
+    }
+}
