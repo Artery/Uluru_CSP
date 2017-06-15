b32d478672abe937942e9585acea9b16e31ee5ed
Johannes Winkler <jw.artery22@web.de>
2017-06-15 13:22:28 +0200

    Refactors creation of custom Decks into methods and removes it from standard deck generation Adds creation of TestDecks

--- Uluru_CSP/Assets/Scripts/DeckGenerator.cs
+++ Uluru_CSP/Assets/Scripts/DeckGenerator.cs
@@ -7,10 +7,15 @@ using System.Linq;
 
 public class DeckGenerator : MonoBehaviour
 {
+    public enum enRuleCardColors { WHITE = 0, PINK = 1, YELLOW = 2, ORANGE = 3, RED = 4, GREEN = 5, BLUE = 6, BLACK = 7 }
+
     [SerializeField]
     private List<DifficultyRuleCardCollection> m_RuleCardPrefabs;
     [SerializeField]
     private Transform m_DeckParentTransform;
+
+    [SerializeField]
+    private bool GeneratePredefinedTestSequences;
 
     private static readonly List<Color> m_Colors = Enum.GetValues(typeof(Color)).Cast<Color>().Where(color => color >= 0).ToList();
     private static readonly System.Random m_RandomNumberGenerator = new System.Random();
@@ -25,6 +30,28 @@ public class DeckGenerator : MonoBehaviour
     void Awake ()
     {
         Instance = this;
+    }
+
+    private static readonly List<Func<enRuleCardColors, RuleCard>> TestCases = new List<Func<enRuleCardColors, RuleCard>>
+                                                           {
+                                                               DeckGenerator.CreateTestNo_1,
+                                                               DeckGenerator.CreateTestNo_2,
+                                                               DeckGenerator.CreateTestNo_3
+                                                           };
+
+    public static CardCollection GenerateTestDecks()
+    {
+        var deck = new CardCollection();
+
+        foreach (var testCase in TestCases)
+        {
+            foreach (var color in Enum.GetValues(typeof(enRuleCardColors)).Cast<enRuleCardColors>())
+            {
+                deck.Add(testCase(color));
+            }
+        }
+
+        return deck;
     }
 
     public static CardCollection GenerateDeck(Difficulty difficulty)
@@ -44,108 +71,10 @@ public class DeckGenerator : MonoBehaviour
                 for (int i = 0; i < bound; i++)
                 {
                     var index = m_RandomNumberGenerator.Next(0, cardCounter++);
-                    RuleCard ruleCard = null;
-                    if (i == 7)
-                    {
-                        //Debug.Log("BLACK");
-                        ruleCard = Instantiate(Instance.m_RuleCardPrefabs.SelectMany(asdf => asdf.RuleCards).
-                                                        First(yo => yo.RulesetType == enRulesetType.NOT_ADJACENT_NOT_OPPOSITE_SIDE));
-                        ruleCard.transform.SetParent(Instance.m_DeckParentTransform);
-                        ruleCard.Color = Color.YELLOW;
-                        ruleCard.Ruleset.Color = Color.YELLOW;
-                        //deck.Insert(0, ruleCard);
-                        deck.Add(ruleCard);
-                    }
-                    else if (i == 6)
-                    {
-                        //Debug.Log("BLUE");
-                        ruleCard = Instantiate(Instance.m_RuleCardPrefabs.SelectMany(asdf => asdf.RuleCards).
-                                                        First(yo => yo.RulesetType == enRulesetType.AROUND_THE_CORNER));
-                        ruleCard.transform.SetParent(Instance.m_DeckParentTransform);
-                        ruleCard.Color = Color.BLACK;
-                        ruleCard.Ruleset.Color = Color.BLACK;
-                        //deck.Insert(0, ruleCard);
-                        deck.Add(ruleCard);
-                    }
-                    else if (i == 5)
-                    {
-                        //Debug.Log("GREEN");
-                        ruleCard = Instantiate(Instance.m_RuleCardPrefabs.SelectMany(asdf => asdf.RuleCards).
-                                                        First(yo => yo.RulesetType == enRulesetType.AROUND_THE_CORNER));
-                        ruleCard.transform.SetParent(Instance.m_DeckParentTransform);
-                        ruleCard.Color = Color.WHITE;
-                        ruleCard.Ruleset.Color = Color.WHITE;
-                        //deck.Insert(0, ruleCard);
-                        deck.Add(ruleCard);
-                    }
-                    else if (i == 4)
-                    {
-                        //Debug.Log("RED");
-                        ruleCard = Instantiate(Instance.m_RuleCardPrefabs.SelectMany(asdf => asdf.RuleCards).
-                                                        First(yo => yo.RulesetType == enRulesetType.OPPOSITE_SIDE));
-                        ruleCard.transform.SetParent(Instance.m_DeckParentTransform);
-                        ruleCard.Color = Color.BLUE;
-                        ruleCard.Ruleset.Color = Color.BLUE;
-                        //deck.Insert(0, ruleCard);
-                        deck.Add(ruleCard);
-                    }
-                    else if (i == 3)
-                    {
-                        //Debug.Log("ORANGE");
-                        ruleCard = Instantiate(Instance.m_RuleCardPrefabs.SelectMany(asdf => asdf.RuleCards).
-                                                        First(yo => yo.RulesetType == enRulesetType.OPPOSITE_SIDE));
-                        ruleCard.transform.SetParent(Instance.m_DeckParentTransform);
-                        ruleCard.Color = Color.BLUE;
-                        ruleCard.Ruleset.Color = Color.BLUE;
-                        //deck.Insert(0, ruleCard);
-                        deck.Add(ruleCard);
-                    }
-                    else if (i == 2)
-                    {
-                        //Debug.Log("YELLOW");
-                        ruleCard = Instantiate(Instance.m_RuleCardPrefabs.SelectMany(asdf => asdf.RuleCards).
-                                                        First(yo => yo.RulesetType == enRulesetType.ADJACENT));
-                        ruleCard.transform.SetParent(Instance.m_DeckParentTransform);
-                        ruleCard.Color = Color.WHITE;
-                        ruleCard.Ruleset.Color = Color.WHITE;
-                        //deck.Insert(0, ruleCard);
-                        deck.Add(ruleCard);
-                    }
-                    else if (i == 1)
-                    {
-                        //Debug.Log("PINK");
-                        ruleCard = Instantiate(Instance.m_RuleCardPrefabs.SelectMany(asdf => asdf.RuleCards).
-                                                        First(yo => yo.RulesetType == enRulesetType.MINIMUM_DISTANCE_2));
-                        ruleCard.transform.SetParent(Instance.m_DeckParentTransform);
-                        ruleCard.Color = Color.YELLOW;
-                        ruleCard.Ruleset.Color = Color.YELLOW;
+                    var ruleCard = Instantiate(prefab);
+                    ruleCard.transform.SetParent(Instance.m_DeckParentTransform);
 
-                        //deck.Insert(0, ruleCard);
-                        deck.Add(ruleCard);
-                    }
-                    else if (i == 0)
-                    {
-                        //Debug.Log("WHITE");
-                        ruleCard = Instantiate(Instance.m_RuleCardPrefabs.SelectMany(asdf => asdf.RuleCards).
-                                                        First(yo => yo.RulesetType == enRulesetType.NOT_ADJACENT_NOT_OPPOSITE_SIDE));
-                        ruleCard.Color = Color.PINK;
-                        ruleCard.Ruleset.Color = Color.PINK;
-                        ruleCard.transform.SetParent(Instance.m_DeckParentTransform);
-
-                        //deck.Insert(0, ruleCard);
-                        deck.Add(ruleCard);
-                    }
-                    else
-                    {
-                        //Debug.Log("As");
-                        ruleCard = Instantiate(prefab);
-                        ruleCard.transform.SetParent(Instance.m_DeckParentTransform);
-
-                        deck.Insert(index, ruleCard);
-                    }
-                    //var ruleCard = Instantiate(prefab);
-
-
+                    deck.Insert(index, ruleCard);
                 }
             }
             else
@@ -162,5 +91,124 @@ public class DeckGenerator : MonoBehaviour
         }
 
         return deck;
+    }
+
+    private static RuleCard InitializeRuleCard(enRulesetType rulesetType, Color ruleCardColor)
+    {
+        var ruleCard = Instantiate(Instance.m_RuleCardPrefabs.SelectMany(asdf => asdf.RuleCards).
+                                            First(yo => yo.RulesetType == rulesetType));
+        ruleCard.Color = ruleCardColor;
+        ruleCard.Ruleset.Color = ruleCardColor;
+        ruleCard.transform.SetParent(Instance.m_DeckParentTransform);
+
+        return ruleCard;
+    }
+
+    public static RuleCard CreateTestNo_1(enRuleCardColors color)
+    {
+        RuleCard ruleCard = null;
+        switch(color)
+        {
+            case enRuleCardColors.WHITE:
+                ruleCard = InitializeRuleCard(enRulesetType.NOT_ADJACENT_NOT_OPPOSITE_SIDE, Color.PINK);
+                break;
+            case enRuleCardColors.PINK:
+                ruleCard = InitializeRuleCard(enRulesetType.MINIMUM_DISTANCE_2, Color.YELLOW);
+                break;
+            case enRuleCardColors.YELLOW:
+                ruleCard = InitializeRuleCard(enRulesetType.ADJACENT, Color.WHITE);
+                break;
+            case enRuleCardColors.ORANGE:
+                ruleCard = InitializeRuleCard(enRulesetType.OPPOSITE_SIDE, Color.BLUE);
+                break;
+            case enRuleCardColors.RED:
+                ruleCard = InitializeRuleCard(enRulesetType.OPPOSITE_SIDE, Color.BLUE);
+                break;
+            case enRuleCardColors.GREEN:
+                ruleCard = InitializeRuleCard(enRulesetType.AROUND_THE_CORNER, Color.WHITE);
+                break;
+            case enRuleCardColors.BLUE:
+                ruleCard = InitializeRuleCard(enRulesetType.AROUND_THE_CORNER, Color.BLACK);
+                break;
+            case enRuleCardColors.BLACK:
+                ruleCard = InitializeRuleCard(enRulesetType.NOT_ADJACENT_NOT_OPPOSITE_SIDE, Color.YELLOW);
+                break;
+            default:
+                throw new IndexOutOfRangeException();
+        }
+        
+        return ruleCard;
+    }
+
+    public static RuleCard CreateTestNo_2(enRuleCardColors color)
+    {
+        RuleCard ruleCard = null;
+        switch (color)
+        {
+            case enRuleCardColors.WHITE:
+                ruleCard = InitializeRuleCard(enRulesetType.AROUND_THE_CORNER, Color.GREEN);
+                break;
+            case enRuleCardColors.PINK:
+                ruleCard = InitializeRuleCard(enRulesetType.ADJACENT, Color.RED);
+                break;
+            case enRuleCardColors.YELLOW:
+                ruleCard = InitializeRuleCard(enRulesetType.ADJACENT, Color.BLUE);
+                break;
+            case enRuleCardColors.ORANGE:
+                ruleCard = InitializeRuleCard(enRulesetType.NO_PREFERENCE, Color.NONE);
+                break;
+            case enRuleCardColors.RED:
+                ruleCard = InitializeRuleCard(enRulesetType.OPPOSITE_SIDE, Color.BLACK);
+                break;
+            case enRuleCardColors.GREEN:
+                ruleCard = InitializeRuleCard(enRulesetType.AROUND_THE_CORNER, Color.PINK);
+                break;
+            case enRuleCardColors.BLUE:
+                ruleCard = InitializeRuleCard(enRulesetType.NOT_ADJACENT_NOT_OPPOSITE_SIDE, Color.PINK);
+                break;
+            case enRuleCardColors.BLACK:
+                ruleCard = InitializeRuleCard(enRulesetType.MINIMUM_DISTANCE_2, Color.ORANGE);
+                break;
+            default:
+                throw new IndexOutOfRangeException();
+        }
+
+        return ruleCard;
+    }
+
+    public static RuleCard CreateTestNo_3(enRuleCardColors color)
+    {
+        RuleCard ruleCard = null;
+        switch (color)
+        {
+            case enRuleCardColors.WHITE:
+                ruleCard = InitializeRuleCard(enRulesetType.LONG_SIDE, Color.NONE);
+                break;
+            case enRuleCardColors.PINK:
+                ruleCard = InitializeRuleCard(enRulesetType.ADJACENT, Color.WHITE);
+                break;
+            case enRuleCardColors.YELLOW:
+                ruleCard = InitializeRuleCard(enRulesetType.AROUND_THE_CORNER, Color.PINK);
+                break;
+            case enRuleCardColors.ORANGE:
+                ruleCard = InitializeRuleCard(enRulesetType.OPPOSITE_SIDE, Color.RED);
+                break;
+            case enRuleCardColors.RED:
+                ruleCard = InitializeRuleCard(enRulesetType.LONLEY_GROUP, Color.NONE);
+                break;
+            case enRuleCardColors.GREEN:
+                ruleCard = InitializeRuleCard(enRulesetType.MINIMUM_DISTANCE_2, Color.RED);
+                break;
+            case enRuleCardColors.BLUE:
+                ruleCard = InitializeRuleCard(enRulesetType.NOT_ADJACENT_NOT_OPPOSITE_SIDE, Color.GREEN);
+                break;
+            case enRuleCardColors.BLACK:
+                ruleCard = InitializeRuleCard(enRulesetType.BUMERANG_GROUP, Color.NONE);
+                break;
+            default:
+                throw new IndexOutOfRangeException();
+        }
+
+        return ruleCard;
     }
 }
